6ec9206f311f4634e61ef4989c67dc86c73b75f3
Marek Chłopek <72882550+markoseb@users.noreply.github.com>
2021-11-22 17:16:09 +0100

    New field for product form. ProdutsList changes

--- beebloge/products/forms.py
+++ beebloge/products/forms.py
@@ -8,6 +8,7 @@ from flask import flash
 class ProductForm(FlaskForm):
     title = StringField('Nazwa produktu', validators=[DataRequired()])
     category = StringField('Kategoria',validators=[DataRequired()])
+    shortText = CKEditorField('Krótki opis', validators=[DataRequired()])
     text = CKEditorField('Opis', validators=[DataRequired()])
     picture = FileField('Zdjęcie', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Opublikuj')
@@ -15,6 +16,12 @@ class ProductForm(FlaskForm):
     def check_title(self):
         if len(self.title.data) > 35:
             flash('Nazwa produktu nie może być dłuższa niż 35 znaków')
+            # raise ValidationError('Your email has been registered already!')
+            return False
+        return True
+    def check_shortText(self):
+        if len(self.shortText.data) > 200:
+            flash('Krótki opis nie może być dłuższy niż 200 znaków!')
             # raise ValidationError('Your email has been registered already!')
             return False
         return True
